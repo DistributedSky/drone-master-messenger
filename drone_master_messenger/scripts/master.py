3a79cda74c98504eae174d8de5a66a2569c45712
Alexander Krupenkin <mail@akru.me>
2017-07-06 14:24:38 +0300

    Added drone recorder support

--- drone_master_messenger/scripts/master.py
+++ drone_master_messenger/scripts/master.py
@@ -3,6 +3,7 @@
 from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
 from mavros_msgs.srv import WaypointSetCurrent
 from mavros_msgs.msg import State
+from std_srvs.srv import SetBool
 from std_msgs.msg import String, UInt32
 from messenger import messenger_mission_gen
 from messenger import messenger_drone_free
@@ -34,13 +35,22 @@ def takeoff(adapter):
     rospy.wait_for_service(adapter+'/mavros/cmd/takeoff')
     try:
         takeoff = rospy.ServiceProxy(adapter+'/mavros/cmd/takeoff', CommandTOL)
-        takeoff(0,0,0,0,10)
+        takeoff(0,0,0,0,5)
         rospy.loginfo('{0} :: Takeoff'.format(adapter))
         rospy.sleep(10)
     except e:
         print('Service call failed: {0}'.format(e))
 
-states = {}
+def record(enable):
+    rospy.wait_for_service(adapter+'/camera/record')
+    try:
+        record = rospy.ServiceProxy(adapter+'/camera/record', SetBool)
+        if record(enable).success:
+            rospy.loginfo('{0} :: Record > {1} is success'.format(adapter, enable))
+        else:
+            rospy.logerr('{0} :: Record > {1} is fail'.format(adapter, enable))
+    except e:
+        print('Service call failed: {0}'.format(e))
 
 if __name__ == '__main__':
     rospy.init_node('drone_master')
@@ -60,17 +70,31 @@ if __name__ == '__main__':
         def drone_state(msg):
             states[adapter] = msg
 
-        rospy.Subscriber(msg.data+'/mavros/state', State, drone_state)
+        rospy.Subscriber('{0}/mavros/state'.format(adapter), State, drone_state)
         rospy.loginfo('{0} :: State subscribed'.format(adapter))
+
+        def drone_video(msg):
+            messenger_drone_video(adapter, msg.data)
+
+        rospy.Subscriber('{0}/camera/video'.format(adapter), String, drone_video)
+        rospy.loginfo('{0} :: Video subscribed'.format(adapter))
+
+        def drone_thumbnail(msg):
+            messenger_drone_thumbnail(adapter, msg.data)
+
+        rospy.Subscriber('{0}/camera/thumbnail'.format(adapter), String, drone_thumbnail)
+        rospy.loginfo('{0} :: Thumbnail subscribed'.format(adapter))
 
         def drone_mission():
             rospy.loginfo('{0} :: Mission thread started'.format(adapter))
             for point in messenger_mission_gen(adapter):
                 if point < 0:
+                    record(False)
                     set_mode(adapter, 'RTL')
                     messenger_drone_free(adapter)
 
                 if not states[adapter].armed:
+                    record(True)
                     arming(adapter)
                     takeoff(adapter)
 
